19a476a96d2c9f12eed0cbb1e41f02f858fa2fa1
Vladimir Rubin <vavakado@skiff.com>
2024-02-28 19:46:01 +0200

    feat: added ability for new tile types

--- main_scene/game.gd
+++ main_scene/game.gd
@@ -1,23 +1,31 @@
-#extends Node2D
-#
-#const TILE_SCENE = preload("res://tilemap.tscn")
-#var tileground = TILE_SCENE.instantiate()
-#
-#func _ready():
-	#add_sibling(tileground)
-#
-#
-#
-#
-#
-#
-#
-#
-#func gen():
-	#var tileground = TILE_SCENE.instantiate()
-	#tileground.global_position += Vector2(40, 0)
-	#add_sibling(tileground)
-	#10 - 29
-#func _process(delta):
-	#if $Player.global_position.x == tileground.global_position.x + 10 or $Player.global_position.x == tileground.global_position.x + 29:
-		#gen()
+extends Node2D
+class tile_piece:
+	var index: int
+	var variant: int
+	func _init(index, variant):
+		self.index = index
+		self.variant = variant
+		
+const TILE_SCENE = preload("res://tilemap.tscn")
+var spawned_tiles = [tile_piece.new(0, 0)]
+
+func _ready():
+	var tileground = TILE_SCENE.instantiate()
+	$Player.new_tile.connect(gen)
+	add_child(tileground)
+	
+func check_array_for_index(arr, num):
+	for item in arr:
+		if item.index == num:
+			return true
+	return false
+
+func gen(index: int):
+	if !check_array_for_index(spawned_tiles, index):
+		var tileground = TILE_SCENE.instantiate()
+		tileground.global_position += Vector2((index)*640, 0)
+		add_child(tileground)
+		spawned_tiles.append(tile_piece.new(index, 0))
+		
+func _process(delta):
+	pass
