630133d1602a725ccfa03f0ef4890700da126ca2
Vladimir Rubin <vavakado@proton.me>
2024-03-02 20:39:24 +0200

    ayo

--- main_scene/game.gd
+++ main_scene/game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 const Tilemap_start_1 = preload("res://elements/Tilemaps/Tilemap_start_1.tscn")
-const Tilemap_corridor_2_up = preload("res://elements/Tilemaps/Tilemap_corridor_2_up.tscn")
 const Tilemap_corridor_1 = preload("res://elements/Tilemaps/Tilemap_corridor_1.tscn")
-
+const Tilemap_corridor_2_up = preload("res://elements/Tilemaps/Tilemap_corridor_2_up.tscn")
+const Tilemap_corridor_2_down = preload("res://elements/Tilemaps/Tilemap_corridor_2_down.tscn")
+const Tilemap_corner_1_up = preload("res://elements/Tilemaps/Tilemap_corner_1_up.tscn")
+const Tilemap_corner_1_down = preload("res://elements/Tilemaps/Tilemap_corner_1_down.tscn")
 @onready var Camera = $Camera2D
 
 func _ready():
@@ -12,29 +14,32 @@ func _ready():
 	for i in range(5):
 		var be: Array
 		for j in range(5):
-			be.push_back(randi_range(1,2))
+			be.push_back(randi_range(1,6))
 		map.push_back(be)
-		map[0][0] = 0
 		
 	var c = 0
 	var z = 0
 	
 	for i in map:
-		for k in i:
-			if k == 0:
-				var tile = Tilemap_start_1.instantiate()
-				tile.global_position = Vector2(z*560, c*352)
-				add_child(tile)
-			elif k == 1:
-				var tile = Tilemap_corridor_1.instantiate()
-				tile.global_position = Vector2(z*560, c*352)
-				add_child(tile)
-			elif k == 2:
-				var tile = Tilemap_corridor_2_up.instantiate()
-				tile.global_position = Vector2(z*560, c*352)
-				add_child(tile)
+		for variant in i:
+			if variant == 0:
+				spawn_tile(Tilemap_start_1, z, c)
+			elif variant == 1:
+				spawn_tile(Tilemap_corridor_1, z, c)
+			elif variant == 2:
+				spawn_tile(Tilemap_corridor_2_up, z, c)
+			elif variant == 3:
+				spawn_tile(Tilemap_corridor_2_down, z, c)
+			elif variant == 4:
+				spawn_tile(Tilemap_corner_1_up, z, c)
+			elif variant == 5:
+				spawn_tile(Tilemap_corner_1_down, z, c)
 			c += 1
 			print(c)
 			print(z)
 		z += 1
 		c = 0
+func spawn_tile(variant, x, y):
+	var tile = variant.instantiate()
+	tile.global_position = Vector2(x*560, y*352)
+	add_child(tile)
