83ae097ec378005442530e42df0796a7ac7a8d7f
Vladimir Rubin <vavakado@proton.me>
2024-03-12 15:17:19 +0200

    fix: camera movement

--- elements/Tilemaps/Tilemap_pipe_1.gd
+++ elements/Tilemaps/Tilemap_pipe_1.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body):
-	Events.move_camera.emit(global_position)
+	if scale.x == 1:
+		Events.move_camera.emit(global_position)
+	else:
+		Events.move_camera.emit(global_position-Vector2(560, 0))
