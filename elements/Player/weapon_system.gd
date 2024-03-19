95265ffc30d59c295dc6df46eac6e083a5d1cdf1
Vladimir Rubin <vavakado@proton.me>
2024-03-19 19:31:41 +0200

    fix: weapon_system

--- elements/Player/weapon_system.gd
+++ elements/Player/weapon_system.gd
@@ -5,21 +5,29 @@ extends Node2D
 
 var currentWeapon
 var weaponNode
+var actual_weapon_type
 
 
 func _ready():
+	actual_weapon_type = Globals.weapon_type
 	equipWeapon(weaponScene1)
 
 func _process(delta):
 	pass
 
 func _input(event):
-	print(Globals.weapon_type)
-	if Globals.weapon_type == Globals.weapontype.NONE:
-		Globals.weapon_type = Globals.weapontype.PISTOL
-		equipWeapon(weaponScene1)
-	if Globals.weapon_type == Globals.weapontype.BLASTER:
-		equipWeapon(weaponScene2)
+	if actual_weapon_type != Globals.weapon_type:
+		print(Globals.weapon_type)
+		if Globals.weapon_type == Globals.weapontype.NONE:
+			Globals.weapon_type = Globals.weapontype.PISTOL
+			actual_weapon_type = Globals.weapon_type
+			equipWeapon(weaponScene1)
+		if Globals.weapon_type == Globals.weapontype.PISTOL:
+			actual_weapon_type = Globals.weapon_type
+			equipWeapon(weaponScene1)
+		if Globals.weapon_type == Globals.weapontype.BLASTER:
+			actual_weapon_type = Globals.weapon_type
+			equipWeapon(weaponScene2)
 
 func equipWeapon(weaponScene):
 	if weaponNode != null:
