9905a2c6fa3d60ef9e24cb0926a8ac67a76d28bc
orriginalo <originalo1717@gmail.com>
2024-03-25 21:53:11 +0400

    Platformer -> Geometry Dash kupili primerno ot 4 milionaf ludei do 8

--- elements/Player/player.gd
+++ elements/Player/player.gd
@@ -24,36 +24,32 @@ func _ready():
 	Events.entered_E.connect(pressE)
 
 func _physics_process(delta):
-	if not is_on_floor() or jump_count == 1 or jump_count == 2:
-		velocity.y += gravity * delta
-	if Input.is_action_just_pressed("W") and is_on_floor():
-		$jump.play()
-		jump_count = 1
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("W") and not is_on_floor() and jump_count == 1:
-		$double_jump.play()
-		jump_count = 2
-		velocity.y = JUMP_VELOCITY
-	elif is_on_floor():
-		jump_count = 0
-	var direction = Input.get_axis("A", "D")
-	velocity.x = direction * SPEED
+	var direction_y = Input.get_axis("W","S")
+	var direction_x = Input.get_axis("A", "D")
+	velocity.y = direction_y * SPEED
+	velocity.x = direction_x * SPEED
+	if direction_y !=0 and direction_x !=0:
+		velocity.y = direction_y * SPEED/1.41421356237
+		velocity.x = direction_x * SPEED/1.41421356237
 	move_and_slide()
-	if not is_on_floor():
-		$AnimatedSprite2D.scale.y = remap(abs(velocity.y), 0, abs(JUMP_VELOCITY), 0.75, 1.75)
-		$AnimatedSprite2D.scale.x = remap(abs(velocity.y), 0, abs(JUMP_VELOCITY), 1.25, 0.75)
-	else:
-		$AnimatedSprite2D.scale = Vector2(1,1)
+	#if not is_on_floor():
+		#$AnimatedSprite2D.scale.y = remap(abs(velocity.y), 0, abs(JUMP_VELOCITY), 0.75, 1.75)
+		#$AnimatedSprite2D.scale.x = remap(abs(velocity.y), 0, abs(JUMP_VELOCITY), 1.25, 0.75)
+	#else:
+		#$AnimatedSprite2D.scale = Vector2(1,1)
 		
-	
-	if direction < 0 or Vector2(get_global_mouse_position()) < global_position:
-		animation.set_flip_h(true)
-	if direction > 0 or Vector2(get_global_mouse_position()) > global_position:
+	if Vector2(get_global_mouse_position()) > global_position:
 		animation.set_flip_h(false)
-	if direction == 0:
+	if Vector2(get_global_mouse_position()) < global_position:
+		animation.set_flip_h(true)
+	if direction_x < 0:
+		animation.set_flip_h(true)
+	if direction_x > 0:
+		animation.set_flip_h(false)
+	
+		
+	if direction_x == 0 and direction_y == 0:
 		animation.play("idle")
-	elif direction and $weapon_system.weaponNode:
-		animation.play("run_with_gun")
 	else:
 		animation.play("run")
 	
@@ -69,11 +65,10 @@ func _physics_process(delta):
 				$weapon_system/GUI/shopanim.play_backwards()
 				animcount = 1
 	
-	run_sound()
 	gui_ammo()
 	look_w()
 	click_fire()
-	
+		
 func look_w():
 	$weapon_system.weaponNode.look_at(get_global_mouse_position())
 	
@@ -95,15 +90,3 @@ func pressE(state):
 		in_shop_area = false
 		Globals.shop_area_state = false
 		Esprite.hide()
-
-func run_sound():
-	if abs(velocity.x) > 0 and run_count == 0 and is_on_floor():
-		run_count = 1
-		$run.play()
-		$Timer.start()
-	
-
-
-func _on_timer_timeout():
-	run_count = 0
-	run_sound()
